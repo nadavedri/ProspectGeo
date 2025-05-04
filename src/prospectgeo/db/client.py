c1eda2c2d0098a0a89f10a6247ea039dd1619284
nadavedri <nadav052433@gmail.com>
2025-05-04 21:57:11 +0300

    add logs use config and handle exactions

--- src/prospectgeo/db/client.py
+++ src/prospectgeo/db/client.py
@@ -1,18 +1,28 @@
 from psycopg2 import connect
 import os
+from prospectgeo.utils.logging_config import logger  
 
 
 def _get_postgres_connection():
-    return connect(
-        dbname=os.getenv("POSTGRES_DB", "postgres"),
-        user=os.getenv("POSTGRES_USER", "postgres"),
-        password=os.getenv("POSTGRES_PASSWORD", "password"),
-        host=os.getenv("POSTGRES_HOST", "localhost"),
-        port=os.getenv("POSTGRES_PORT", "5432"),
-    )
+    logger.info("Attempting to establish a connection to the PostgreSQL database.")
+    try:
+        connection = connect(
+            dbname=os.getenv("POSTGRES_DB", "postgres"),
+            user=os.getenv("POSTGRES_USER", "postgres"),
+            password=os.getenv("POSTGRES_PASSWORD", "password"),
+            host=os.getenv("POSTGRES_HOST", "localhost"),
+            port=os.getenv("POSTGRES_PORT", "5432"),
+        )
+        logger.info("Successfully connected to the PostgreSQL database.")
+        return connection
+    except Exception as e:
+        logger.error("Failed to connect to the PostgreSQL database: %s", e)
+        raise
 
 
 def get_db_client(db_type: str):
+    logger.info("Requesting database client for DB type: %s", db_type)
     if db_type == "postgres":
         return _get_postgres_connection()
+    logger.error("Unsupported DB type requested: %s", db_type)
     raise ValueError(f"Unsupported DB type: {db_type}")
