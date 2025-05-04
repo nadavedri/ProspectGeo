b16583b437cb0d5de0fe6ea2ab21e2e141d51a69
nadavedri <nadav052433@gmail.com>
2025-05-04 14:08:44 +0300

    db manager for more dbs in the future

---
+++ src/prospectgeo/db/__init__.py
@@ -0,0 +1,4 @@
+from .db_manager import create_all_schemas
+
+def setup_databases():
+    create_all_schemas()
---
+++ src/prospectgeo/db/db_manager.py
@@ -0,0 +1,6 @@
+from .client import get_db_client
+from .schemas.postgres.qualification_results_schema import create_qualification_results_schema
+ 
+def create_all_schemas():
+    postgres_conn = get_db_client("postgres")
+    create_qualification_results_schema(postgres_conn)
