ef0cf2f72cd5dda41b272dfa4bff2b322954554b
nadavedri <nadav052433@gmail.com>
2025-05-04 21:58:38 +0300

    add logs and error handling

--- src/prospectgeo/db/db_manager.py
+++ src/prospectgeo/db/db_manager.py
@@ -2,8 +2,20 @@ from .client import get_db_client
 from .schemas.postgres.qualification_results_schema import (
     create_qualification_results_schema,
 )
+from prospectgeo.utils.logging_config import logger  
 
 
 def create_all_schemas():
-    postgres_conn = get_db_client("postgres")
-    create_qualification_results_schema(postgres_conn)
+    logger.info("Starting schema creation process.")
+    try:
+        postgres_conn = get_db_client("postgres")
+        logger.info("Successfully obtained PostgreSQL connection.")
+        create_qualification_results_schema(postgres_conn)
+        logger.info("Successfully created qualification results schema.")
+    except Exception as e:
+        logger.error("An error occurred during schema creation: %s", e)
+        raise
+    finally:
+        if 'postgres_conn' in locals() and postgres_conn:
+            postgres_conn.close()
+            logger.info("PostgreSQL connection closed.")
