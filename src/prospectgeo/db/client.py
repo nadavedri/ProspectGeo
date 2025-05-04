e27cb35ca68c10f8357cb8be03b945c3d5a3da51
nadavedri <nadav052433@gmail.com>
2025-05-04 14:06:48 +0300

    create postgres connection and client

---
+++ src/prospectgeo/db/client.py
@@ -0,0 +1,16 @@
+from psycopg2 import connect
+import os
+
+def get_postgres_connection():
+    return connect(
+        dbname= os.getenv("POSTGRES_DB", "postgres"),
+        user=os.getenv("POSTGRES_USER", "postgres"),
+        password=os.getenv("POSTGRES_PASSWORD", "password"),
+        host=os.getenv("POSTGRES_HOST", "localhost"),
+        port=os.getenv("POSTGRES_PORT", "5432")
+    )
+
+def get_db_client(db_type: str):
+    if db_type == "postgres":
+        return get_postgres_connection()
+    raise ValueError(f"Unsupported DB type: {db_type}")
